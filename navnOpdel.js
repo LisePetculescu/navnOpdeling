223854684dbdd82033b39d5cb97df7cddc2d719d
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-03-16 11:23:59 +0100

    kk

--- navnOpdel.js
+++ navnOpdel.js
@@ -1,22 +1,54 @@
 "use-strict";
 
-const fullName = "Lise Freja Jensen Petculescu";
+function writeNameParts(fullName) {
+  //   const fullName = "LiSe Freja Jensen PEtcUleScu";
 
-const spaceFirst = fullName.indexOf(" ") + 1;
-console.log(spaceFirst);
+  const spaceFirst = fullName.indexOf(" ") + 1;
+  console.log(spaceFirst);
 
-const spaceEnd = fullName.lastIndexOf(" ") + 1;
-console.log(spaceEnd);
+  const spaceEnd = fullName.lastIndexOf(" ") + 1;
+  console.log(spaceEnd);
 
-const firstName = fullName.substring(0, spaceFirst - 1);
-console.log(`#${firstName}£`);
+  const firstName = capitalized(fullName.substring(0, spaceFirst - 1));
+  console.log(`#${firstName}£`);
 
-const middleName = fullName.substring(spaceFirst, spaceEnd - 1);
-console.log(`#${middleName}#`);
+  let middleName = capitalized(fullName.substring(spaceFirst, spaceEnd - 1));
+  console.log(`#${middleName}#`);
 
-const lastName = fullName.substring(spaceEnd);
-console.log(`#${lastName}#`);
+  const lastName = capitalized(fullName.substring(spaceEnd));
+  console.log(`#${lastName}#`);
 
-console.log(`Firstname: ${firstName}`);
-console.log(`Middlename: ${middleName}`);
-console.log(`Lastname: ${lastName}`);
+  if (middleName === " ") {
+    middleName = undefined;
+  }
+
+  console.log(`Firstname: ${firstName}`);
+  console.log(`Middlename: ${middleName}`);
+  console.log(`Lastname: ${lastName}`);
+  console.log({ firstName, middleName, lastName });
+
+  return { firstName, middleName, lastName };
+}
+
+function capitalized(myName) {
+  const capFirst = myName.substring(0, 1).toUpperCase();
+  console.log(capFirst);
+
+  const smallAfter = myName.substring(1).toLowerCase();
+  console.log(smallAfter);
+
+  const newName = `${capFirst}${smallAfter}`;
+  console.log(newName);
+
+  return newName;
+}
+
+writeNameParts("LiSe FReJA JeNsen PeTcUlescu");
+
+// stack overflow fejl - just for fun
+// function a() {
+//   console.log("1");
+//   a();
+//   console.log("2");
+// }
+// a();
